185990623f1e004d33ebb5b2b188a004a00dca19
mahmoud tarek <mahmoudtark556@gmail.com>
2025-04-09 15:07:59 +0200

    queries

---
+++ Northwind/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Projection projection = new Projection();
+			projection.Query38();
+			Console.WriteLine();
+			Console.WriteLine();
+			Console.WriteLine();
+			Console.WriteLine();
+			Console.WriteLine();
+			Console.WriteLine();
+			Console.WriteLine();
+			Console.WriteLine();
+			Console.WriteLine();
+			Console.WriteLine();
+			projection.Query39();
+		}
+	}
+}
